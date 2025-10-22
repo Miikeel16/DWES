0140f066ca5a1fd92391750d4148c16cff6afb14
Miikeel16 <mikel.garcia06@somo.eus>
2025-10-22 13:39:51 +0200

    terminado

--- F1/obtenerPiloto.php
+++ F1/obtenerPiloto.php
@@ -1,27 +1,59 @@
 <?php
 function obtenerPiloto($numeroPiloto) {
-    $url = "https://api.openf1.org/v1/drivers?driver_number=$numeroPiloto&session_key=latest";
-
     $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.openf1.org/v1/drivers?driver_number=$numeroPiloto&session_key=latest",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+    ));
 
-    $respuesta = curl_exec($curl);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
 
-    if ($respuesta === false) {
-        curl_close($curl);
+    if ($err) {
+        echo "<p>Error curl #:" . htmlspecialchars($err) . "</p>";
         return null;
     }
 
-    curl_close($curl);
-    return json_decode($respuesta, true)[0] ?? null;
+    $data = json_decode($response, true);
+    return $data[0] ?? null;
 }
 
-$piloto16 = obtenerPiloto(16);
-$piloto44 = obtenerPiloto(44);
+$pilotos = [
+    obtenerPiloto(16),
+    obtenerPiloto(44),
+];
 
-print_r($piloto16);
-print_r($piloto44);
+echo "<table border='1' cellpadding='8' cellspacing='0' style='border-collapse: collapse; text-align: center;'>";
+echo "<tr style='background-color: #ddd;'>
+        <th>Número</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Equipo</th>
+        <th>Abreviatura</th>
+      </tr>";
+
+foreach ($pilotos as $p) {
+    if ($p) {
+        echo "<tr>
+                <td>{$p['driver_number']}</td>
+                <td>{$p['first_name']}</td>
+                <td>{$p['last_name']}</td>
+                <td>{$p['team_name']}</td>
+                <td>{$p['name_acronym']}</td>
+              </tr>";
+    } else {
+        echo "<tr><td colspan='5'>No se pudo obtener información del piloto</td></tr>";
+    }
+}
+
+echo "</table>";
 ?>
 
 <!-- 
